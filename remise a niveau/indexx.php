2d508411c18e6b134ca01594e3383f4ebe1c5454
Imane <113670994+imanemeradi@users.noreply.github.com>
2022-10-25 15:25:39 +0200

    Add files via upload

--- remise a niveau/indexx.php
+++ remise a niveau/indexx.php
@@ -1,2 +1,24 @@
 <?php
-echo 'hello world'; ?>
+
+require_once('views/base.php');
+
+echo 'hello world'; 
+
+switch($_SERVER['REQUEST_URI']) {
+    case '/':
+        echo 'ACCUEIL';
+        break;
+    case '/contact':
+        echo 'CONTACT';
+        break;
+    
+}
+
+/* if ($_SERVER['REQUEST_URI']==='/') {
+    echo 'ACCUEIL';
+}
+elseif($_SERVER['REQUEST_URI']==='/contact'){
+    echo 'CONTACT';
+}  */
+
+?>
